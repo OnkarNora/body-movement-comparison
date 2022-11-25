adf299b76bc0025087186edcfc17446df6855187
Onkar Nora <onora155@gmail.com>
2022-11-25 10:41:44 +0530

    added methods to choose from with method.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import compare from './posenet';
-import VideoInput from './VideoInput';
-import { useState } from 'react';
-import model_video from './m_hands_up_2.mp4'
+import Method from './VideoSpecifics/Method';
+import model_video from './model_videos/aniket_hands_up_4.mp4'
 
 function App() {
 
-  const [video,setVideo] = useState();
-
   return (
 
-    <div className="App">
-      {/* <img id='image' src={require('./Capture.PNG')} width="372" height="495"/> */}
-      <VideoInput setVideo={setVideo}  width={640} height={480} />
-      {/* <VideoInput setVideo={setVideo}  width={372} height={495} /> */}
-      <div className="VideoInput">
-      <video
-          id = 'model_video'
-          className="VideoInput_video"
-          width={640}  
-          height={480}
-        ><source src={model_video}
-        type="video/mp4"></source></video>
+    <div>
+        <Method model_video={model_video} />
       </div>
-      
-      {/* <img id='model_image' src={require('./Capture3.PNG')} width="372" height="495"/> */}
-      <button onClick={()=>{compare(document.getElementById('video'),document.getElementById('model_video'))}}>Check</button>
-    </div>
   );
 }
 
---
+++ src/VideoSpecifics/Method.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react'
+import './Method.css'
+import UploadVideo from './UploadVideo'
+import RecordVideo from './RecordVideo'
+import { useState } from 'react'
+
+function Method(props) {
+
+    const [step, setStep] = useState(1);
+
+    useEffect(() => {
+        setStep(1);
+    }, [])
+    
+
+    switch (step) {
+        case 1:
+            return (
+                <div id="cards_landscape_wrap-2" >
+                    <div className="container"  >
+                        <div className="row" >
+                        <h1>Choose a Method</h1>
+                            <div style={{display:'flex',justifyContent:'space-between',}} >
+                                
+                                <div className="col-xs-12  col-lg-4">
+                                    <button onClick={()=>{setStep(2)}}>
+                                        <div className="card-flyer">
+                                            <div className="text-box">
+                                                <div className="image-box">
+                                                    <img src="https://cdn.pixabay.com/photo/2016/11/30/18/14/download-1873539_960_720.png" alt="" />
+                                                </div>
+                                                <div className="text-container">
+                                                    <h6>Upload a Video</h6>
+                                                    <p>Choose a video from your local computer to test it with the standard video, make sure the video file is having extension .mp4 and is of minimum 360p.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </button>
+                                </div>
+                                <div className="col-xs-12  col-lg-4 ">
+                                    <button onClick={()=>{setStep(3)}}>
+                                        <div className="card-flyer">
+                                            <div className="text-box">
+                                                <div className="image-box">
+                                                    <img src="https://cdn.pixabay.com/photo/2017/01/25/17/35/camera-2008489_960_720.png" alt="" />
+                                                </div>
+                                                <div className="text-container">
+                                                    <h6>Record Video</h6>
+                                                    <p>Record a video with the camera connected to this computer. make sure the lighting is good and all body parts are visible on the screen.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        case 2:
+            return (
+                <UploadVideo model_video={props.model_video} />
+            )
+        case 3:
+            return (
+                <RecordVideo/>
+            )
+    }
+}
+
+export default Method
