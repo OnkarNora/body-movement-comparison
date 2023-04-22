94081de2a9f9372c69456614d138905ef1436fd4
Chinmay Vijapure <vijapurechinmay@gmail.com>
2023-04-22 20:14:55 +0530

    main_carousel

--- src/pages/Dashboards.js
+++ src/pages/Dashboards.js
@@ -1,4 +1,7 @@
 import React from "react";
+import {videos} from '../VideoSpecifics/videos';
+import CustomCard from "../components/CustomCard";
+import Carousel from 'carousel-react-rcdev'
 
 export const Dashboards = () => {
 return (
@@ -8,10 +11,25 @@ return (
 );
 };
 
-export const DashboardsOne = () => {
+export const DashboardsOne = ({isSidebarOpen}) => {
 return (
-	<div className="Dashboards">
+	<div className={isSidebarOpen ? "dashboards left-marging":"dashboards"}>
 	<h1>Ongoing exercise</h1>
+	<div>
+	<Carousel>
+		{videos.map((val,idx)=>{return (<CustomCard img={val.img} title={val.title} description={val.description} id={idx} />)})}
+		{videos.map((val,idx)=>{return (<CustomCard img={val.img} title={val.title} description={val.description} id={idx} />)})}
+    </Carousel>
+	</div>
+
+	<h1>Upcoming exercise</h1>
+	<div>
+	<Carousel>
+		{videos.map((val,idx)=>{return (<CustomCard img={val.img} title={val.title} description={val.description} id={idx} />)})}
+		{videos.map((val,idx)=>{return (<CustomCard img={val.img} title={val.title} description={val.description} id={idx} />)})}
+    </Carousel>
+	</div>
+
 	</div>
 );
 };
